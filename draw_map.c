0997f7e36cb8b80d0db2aba470da32cc59710bd0
jeongyunnim <jeongyun7521@gmail.com>
2023-04-19 21:00:35 +0900

    벽 그리기 시작

--- draw_map.c
+++ draw_map.c
@@ -6,7 +6,7 @@
 /*   By: jeseo <jeseo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/15 19:44:32 by jeseo             #+#    #+#             */
-/*   Updated: 2023/04/18 19:43:44 by jeseo            ###   ########.fr       */
+/*   Updated: 2023/04/19 20:55:04 by jeseo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,14 @@
 
 int	draw_map(t_info *info)
 {
-	t_mbase	mbase;
-	double	perp_wall_dist;
+	t_ray	ray;
 	int		i;
 
-	init_map_base(info, &mbase);
 	i = 0;
-	while (i <= SCREEN_W)
+	while (i < SCREEN_W)
 	{
-		perp_wall_dist = cal_perp_dist(info, &mbase, 2 * i / (double)SCREEN_W - 1);
-		//draw_use_mlx
+		cal_perp_dist(info, &info->mbase, &ray, 2 * i / (double)SCREEN_W - 1);
+		draw_one_line(info, &info->mbase, ray, i);
 		i++;
 	}
 	return (0);
--- draw_one_line.c
+++ draw_one_line.c
@@ -6,24 +6,47 @@
 /*   By: jeseo <jeseo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 20:19:34 by jeseo             #+#    #+#             */
-/*   Updated: 2023/04/18 20:32:48 by jeseo            ###   ########.fr       */
+/*   Updated: 2023/04/19 19:59:55 by jeseo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int	draw_one_line(t_info *info, double perp_dist)
+void	color_one_line(t_info *info, t_line line, t_side color, int i)
 {
-	int	wall_height;
-	int	line_bottom;
-	int	line_top;
+	int j;
 
-	wall_height = SCREEN_H / perp_dist;//이게 뭔데
-	line_bottom = (SCREEN_H / 2) - (wall_height / 2); //이걸 왜 이렇게 하는 거지?
-	if (line_bottom < 0)
-		line_bottom = 0;
-	line_top = (SCREEN_H / 2) + (wall_height / 2);
-	if (line_top >= SCREEN_H) // 여기에만 등호가 들어가는 이유는? 픽셀도 index가 0부터 시작하는 배열인가?
-		line_top = SCREEN_H - 1;
+	j = 0;
+	while (j < SCREEN_H)
+	{
+		if (j < line.bottom)
+			mlx_pixel_put(info->mlx, info->win_mlx, i, j, F_SIDE);
+		else if (j < line.top)
+			mlx_pixel_put(info->mlx, info->win_mlx, i, j, color);
+		else
+			mlx_pixel_put(info->mlx, info->win_mlx, i, j, C_SIDE);
+		j++;
+	}
+}
+
+int	draw_one_line(t_info *info, t_mbase *mbase, t_ray ray, int i)
+{
+	t_line	line;
+
+	line.wall_height = SCREEN_H / ray.perp_dist;
+	line.bottom = (SCREEN_H / 2) - (line.wall_height / 2);
+	if (line.bottom < 0)
+		line.bottom = 0;
+	line.top = (SCREEN_H / 2) + (line.wall_height / 2);
+	if (line.top >= SCREEN_H)
+		line.top = SCREEN_H - 1;
+	if (ray.side == 0 && ray.step.x == -1)
+		color_one_line(info, line, E_SIDE, i);
+	else if (ray.side == 0 && ray.step.x == 1)
+		color_one_line(info, line, W_SIDE, i);
+	else if (ray.side == 1 && ray.step.y == -1)
+		color_one_line(info, line, N_SIDE, i);
+	else
+		color_one_line(info, line, S_SIDE, i);
 	return (0);
 }
