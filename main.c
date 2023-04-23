01b8169eb0d86177236659a8c2686441f1f9fae1
jeongyunnim <jeongyun7521@gmail.com>
2023-04-24 06:28:41 +0900

    img 초기화 함수 추가 + 에러처리

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jeseo <jeseo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/01 17:00:12 by jeseo             #+#    #+#             */
-/*   Updated: 2023/04/23 22:12:49 by jeseo            ###   ########.fr       */
+/*   Updated: 2023/04/24 04:50:47 by jeseo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,18 +34,8 @@ int	main(int argc, char *argv[])
 	}
 	close(fd);
 	init_map_base(&info, &info.mbase);
-
-	int temp;
-
-	info.textures[N_SIDE].img = mlx_xpm_file_to_image(info.mlx, info.north, &temp, &temp);
-	info.textures[S_SIDE].img = mlx_xpm_file_to_image(info.mlx, info.south, &temp, &temp);
-	info.textures[W_SIDE].img = mlx_xpm_file_to_image(info.mlx, info.west, &temp, &temp);
-	info.textures[E_SIDE].img = mlx_xpm_file_to_image(info.mlx, info.east, &temp, &temp);
-	info.textures[N_SIDE].addr = mlx_get_data_addr(info.textures[N_SIDE].img, &info.textures[N_SIDE].bits_per_pixel, &info.textures[N_SIDE].line_length, &info.textures[N_SIDE].endian);
-	info.textures[S_SIDE].addr = mlx_get_data_addr(info.textures[S_SIDE].img, &info.textures[S_SIDE].bits_per_pixel, &info.textures[S_SIDE].line_length, &info.textures[S_SIDE].endian);
-	info.textures[W_SIDE].addr = mlx_get_data_addr(info.textures[W_SIDE].img, &info.textures[W_SIDE].bits_per_pixel, &info.textures[W_SIDE].line_length, &info.textures[W_SIDE].endian);
-	info.textures[E_SIDE].addr = mlx_get_data_addr(info.textures[E_SIDE].img, &info.textures[E_SIDE].bits_per_pixel, &info.textures[E_SIDE].line_length, &info.textures[E_SIDE].endian);
-
+	if (init_img(&info) == ERROR || get_img_address(&info) == ERROR)
+		return (put_error("Error\nTexture file error\n"));
 	draw_map(&info);
 	print_image(&info);
 	mlx_loop_hook(info.mlx, main_loop, &info);
