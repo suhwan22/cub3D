1b50a393f45b25e56c7c0101e2b2316117d24bee
suhwan <tnghkswkd123@naver.com>
2023-05-06 22:05:05 +0900

    fuel 추가하고 G에 도착시 goal 출력되고 종료, 또는 fuel소진시 종료

--- bonus/init_racing_mode_bonus.c
+++ bonus/init_racing_mode_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jeseo <jeseo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 15:10:42 by jeseo             #+#    #+#             */
-/*   Updated: 2023/05/06 16:55:12 by jeseo            ###   ########.fr       */
+/*   Updated: 2023/05/06 21:52:20 by suhkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ int init_racing_mode(t_info *info)
 	info->mbase.map.x = info->racing_init.x;
 	info->mbase.map.y = info->racing_init.y;
 	info->mbase.pos.x = info->mbase.map.x + 0.5;
+	info->racing_start = info->mbase.map.x + 0.5;
 	info->mbase.pos.y = info->mbase.map.y + 0.5;
     info->racing_flag = 1;
 	init_dir(&info->mbase, 1, 0);
