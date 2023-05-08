ed9e66a74355260fda4e4b8a91da57fde529a939
suhwan <tnghkswkd123@naver.com>
2023-05-08 22:25:44 +0900

    핸들 racing mode 들어가기 전에 돌아가있는거 수정 그러나 덜함

--- bonus/mouse_handler_bonus.c
+++ bonus/mouse_handler_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jeseo <jeseo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 20:14:28 by jeseo             #+#    #+#             */
-/*   Updated: 2023/05/08 20:11:18 by suhkim           ###   ########.fr       */
+/*   Updated: 2023/05/08 22:23:23 by suhkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,25 @@ int	mouse_handler(int x, int y, t_info *info)
 
 	if (info->flag.racing == 1)
 		return (0);
-	if (y == 0)
-		y += 1;
-	if (x > SCREEN_W / 5 * 4)
+	if (info->flag.wait == 0)
 	{
-		set_inputs(&info->input[INPUT_LEFT], &info->input[INPUT_RIGHT], 0, 1);
-		flag = 1;
-	}
-	else if (x < SCREEN_W / 5)
-	{
-		set_inputs(&info->input[INPUT_RIGHT], &info->input[INPUT_LEFT], 0, 1);
-		flag = 1;
-	}
-	else if (flag == 1)
-	{
-		set_inputs(&info->input[INPUT_RIGHT], &info->input[INPUT_LEFT], 0, 0);
-		flag = 0;
+		if (y == 0)
+			y += 1;
+		if (x > SCREEN_W / 5 * 4)
+		{
+			set_inputs(&info->input[INPUT_LEFT], &info->input[INPUT_RIGHT], 0, 1);
+			flag = 1;
+		}
+		else if (x < SCREEN_W / 5)
+		{
+			set_inputs(&info->input[INPUT_RIGHT], &info->input[INPUT_LEFT], 0, 1);
+			flag = 1;
+		}
+		else if (flag == 1)
+		{
+			set_inputs(&info->input[INPUT_RIGHT], &info->input[INPUT_LEFT], 0, 0);
+			flag = 0;
+		}
 	}
 	return (0);
 }
